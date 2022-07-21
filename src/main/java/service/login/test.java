db3204554e10f8f02703b24e5f2ef9c167c6b4be
2power9 <huenhu0512@gmail.com>
2022-07-21 15:17:16 +0700

    fix: remove unneed file from @truongnh28

--- src/main/java/service/login/test.java
+++ src/main/java/service/login/test.java
@@ -6,15 +6,15 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class test {
-    public static void main(String[] args) throws URISyntaxException {
-        HttpRequest.newBuilder(new URI("https://postman-echo.com/get"));
-
-        HttpRequest.newBuilder()
-                .uri(new URI("https://postman-echo.com/get"));
-                
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(new URI("https://postman-echo.com/post"))
-                .POST(HttpRequest.noBody())
-                .build();
-    }
+//    public static void main(String[] args) throws URISyntaxException {
+//        HttpRequest.newBuilder(new URI("https://postman-echo.com/get"));
+//
+//        HttpRequest.newBuilder()
+//                .uri(new URI("https://postman-echo.com/get"));
+//
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(new URI("https://postman-echo.com/post"))
+//                .POST(HttpRequest.noBody())
+//                .build();
+//    }
 }
