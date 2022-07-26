80ae290cfb0cc822663dfd4bea6efc345dbb7e2a
nguyenvanxuanvu <nguyenvanxuanvu231@gmail.com>
2022-07-26 22:02:17 +0700

    update return data - zp trans token

--- src/main/java/pay2park/model/payment/ResponseOrderData.java
+++ src/main/java/pay2park/model/payment/ResponseOrderData.java
@@ -4,11 +4,15 @@ public class ResponseOrderData {
     private int returnCode;
     private String appTransId;
     private String orderUrl;
+    private String zp_trans_token;
 
-    public ResponseOrderData(int returnCode, String appTransId, String orderUrl) {
+    public ResponseOrderData(int returnCode, String appTransId, String orderUrl, String zp_trans_token) {
         this.returnCode = returnCode;
         this.appTransId = appTransId;
         this.orderUrl = orderUrl;
+        this.zp_trans_token = zp_trans_token;
+
+
     }
 
     public int getReturnCode() {
@@ -33,5 +37,13 @@ public class ResponseOrderData {
 
     public void setOrderUrl(String orderUrl) {
         this.orderUrl = orderUrl;
+    }
+
+    public String getZp_trans_token() {
+        return zp_trans_token;
+    }
+
+    public void setZp_trans_token(String zp_trans_token) {
+        this.zp_trans_token = zp_trans_token;
     }
 }
