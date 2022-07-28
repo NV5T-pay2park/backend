08659adb99d2a50bb488f63c65177182017315f4
nguyenvanxuanvu <nguyenvanxuanvu231@gmail.com>
2022-07-28 10:49:45 +0700

    update search and filter

--- src/main/java/pay2park/repository/TicketsRepository.java
+++ src/main/java/pay2park/repository/TicketsRepository.java
@@ -2,12 +2,15 @@ package pay2park.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pay2park.model.entityFromDB.EndUser;
 import pay2park.model.entityFromDB.ParkingLot;
 import pay2park.model.entityFromDB.Ticket;
 
+import java.time.Instant;
 import java.util.List;
 
 @Repository
@@ -16,7 +19,4 @@ public interface TicketsRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> getAllTicketByEndUserID(EndUser endUser);
     @Query(value = "SELECT ticket FROM Ticket ticket WHERE ticket.endUser = ?1 AND ticket.parkingLot = ?2 AND ticket.licensePlates = ?3 AND ticket.checkOutTime IS NULL")
     List<Ticket> getTicketByEndUserIDAndParkingLot(EndUser endUser, ParkingLot parkingLot, String licensePlates);
-    @Modifying
-    @Query(value = "UPDATE tickets SET check_out_time = :time WHERE ticket_id = :ticketId", nativeQuery = true)
-    void updateTicketStatus(@Param("ticketId") Long ticketId,@Param("time") Instant time);
 }
