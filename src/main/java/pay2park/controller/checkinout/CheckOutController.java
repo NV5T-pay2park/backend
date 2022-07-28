7fae3b9581d989d9e2732abf56894b2f1d762175
nguyenvanxuanvu <nguyenvanxuanvu231@gmail.com>
2022-07-29 01:04:10 +0700

    ud

--- src/main/java/pay2park/controller/checkinout/CheckOutController.java
+++ src/main/java/pay2park/controller/checkinout/CheckOutController.java
@@ -2,6 +2,7 @@ package pay2park.controller.checkinout;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Timed;
 import org.springframework.web.bind.annotation.*;
 import pay2park.model.ResponseObject;
 import pay2park.model.checkinout.CheckOutData;
@@ -9,12 +10,14 @@ import pay2park.model.checkinout.PreCheckOutData;
 import pay2park.service.checkinout.CheckOutService;
 import pay2park.service.ticket.TicketService;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
+
 public class CheckOutController {
     @Autowired
     CheckOutService checkOutService;
