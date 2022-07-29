eb2d33f248084e2283b83824f6f0c20943cab579
2power9 <huenhu0512@gmail.com>
2022-07-30 00:31:05 +0700

    feat: add merchant employee login API

--- src/main/java/pay2park/controller/login/EndUserLoginController.java
+++ src/main/java/pay2park/controller/login/EndUserLoginController.java
@@ -4,18 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pay2park.model.ResponseObject;
-import pay2park.model.login.LoginData;
-import pay2park.service.login.LoginService;
+import pay2park.model.login.EndUserLoginData;
+import pay2park.service.login.EndUserLoginService;
 
 @RestController
 @CrossOrigin
-public class LoginController {
+public class EndUserLoginController {
     @Autowired
-    LoginService loginService;
+    EndUserLoginService loginService;
 
     @GetMapping("/api/loginenduser")
     public ResponseEntity catchNewLoginEndUser(@RequestParam(value = "zlpId", defaultValue = "") String zlpID) {
-        ResponseObject res = loginService.login(new LoginData(zlpID));
+        ResponseObject res = loginService.login(new EndUserLoginData(zlpID));
         return ResponseEntity.status(res.getStatus()).body(res);
     }
 }
