d99c7418a1697610d0ebea4725ca6da165d5c9d5
truongnh28 <nguyentruongjr@gmail.com>
2022-07-29 17:38:32 +0700

    BREAKING CHANGE: :3

--- src/main/java/pay2park/controller/login/LoginController.java
+++ src/main/java/pay2park/controller/login/LoginController.java
@@ -1,6 +1,7 @@
 package pay2park.controller.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pay2park.model.ResponseObject;
@@ -13,6 +14,10 @@ public class LoginController {
     @Autowired
     LoginService loginService;
 
+    @GetMapping("/")
+    public ResponseEntity<ResponseObject> test() {
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject(HttpStatus.OK, "cc", "ok"));
+    }
     @GetMapping("/api/loginenduser")
     public ResponseEntity catchNewLoginEndUser(@RequestParam(value = "zlpId", defaultValue = "") String zlpID) {
         ResponseObject res = loginService.login(new LoginData(zlpID));
