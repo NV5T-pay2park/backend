033c3cdbe75ef5f91718a07384b48d375f02d3bf
truongnh28 <nguyentruongjr@gmail.com>
2022-07-31 09:06:05 +0700

    BREAKING CHANGE: check bug

--- src/main/java/pay2park/controller/image/ImageController.java
+++ src/main/java/pay2park/controller/image/ImageController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pay2park.model.ResponseObject;
-import pay2park.model.entityFromDB.PaymentUrl;
 import pay2park.service.image.ImageService;
 
 @RestController
