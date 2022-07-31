581b817284668da2fcf283bd7cfc4dbdc72cdac7
truchnt <truchnt@vng.com.vn>
2022-07-31 15:44:43 +0700

    add API get ticket

--- src/main/java/pay2park/controller/ticket/TicketController.java
+++ src/main/java/pay2park/controller/ticket/TicketController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pay2park.model.ResponseObject;
-import pay2park.repository.VehicleTypeRepository;
 import pay2park.service.ticket.TicketService;
 
 @RestController
